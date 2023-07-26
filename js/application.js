290e364aabb95c3df2cdb924bfb92645d51f0172
Josh Petty <jpetty.develop@pm.me>
2023-07-26 15:02:25 -0400

    finished codeing remining JS elements

--- js/application.js
+++ js/application.js
@@ -1,24 +1,60 @@
 var sum = function (acc, x) { return acc + x; };
 
-var updateSubtotal = function (ele) {
-  var price = parseFloat($(ele).children('.price').text());
+var updateSubtotalAndTotal = function () {
+  var subtotalsValues = [];
+
+  var updateSubtotal = function (ele) {
+    var price = parseFloat($(ele).children('.price').text().slice(1));
     var qty = parseFloat($(ele).find('.qty input').val());
 
     var subtotal = price * qty;
-    $(ele).children('.itemSub').html(subtotal);
+    $(ele).children('.itemSub').html(`$${subtotal}`);
 
     return subtotal;
-}
-
-$(document).ready(function () {
-  var subtotalsValues = [];
+  }  
 
   $('tbody tr').each(function (i, ele) {
     var subtotal = updateSubtotal(ele);
     subtotalsValues.push(subtotal);
-    console.log(subtotalsValues);
 
     var cartTotal = subtotalsValues.reduce(sum);
     $('#cartTotal').html(cartTotal);
+    });
+}
+
+$(document).ready(function () {
+  updateSubtotalAndTotal();
+
+  $(document).on('click', '.btn.remove', function (event) {
+    $(this).closest('tr').remove();
+    updateSubtotalAndTotal();
+    if ($('tbody tr').length === 0) {
+      $('#cartTotal').html(0);
+    }
+  });
+
+  var timeout;
+  $(document).on('input', 'tr input', function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      updateSubtotalAndTotal();
+    }, 500);
+  });
+
+  $('#addItem').on('submit', function (event) {
+    event.preventDefault();
+    var newItem = $(this).children(' [name=item] ').val();
+    var newPrice = $(this).children(' [name=price] ').val();
+    
+    $('tbody').append('<tr>' + 
+      '<td class="item">' + newItem + '</td>' +
+      '<td class="price">$' + newPrice + '</td>' + 
+      '<td class="qty"><strong>QTY</strong><input type="number" value="0"/><button class="btn btn-light btn-sm remove">Remove</button></td>' + 
+      '<td class="itemSub"></td>' + 
+      '</tr>');
+
+    $(this).children(' [name=item] ').val('');
+    $(this).children(' [name=price] ').val('');  
+    updateSubtotalAndTotal();
   });
 });
